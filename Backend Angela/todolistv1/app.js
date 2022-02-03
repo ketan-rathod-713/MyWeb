5c01fa8b696a5667c142facb8454fed7fbf65ef5
ketan <ketanrtd1@gmail.com>
2022-02-03 22:13:07 +0530

    applied css changes using serving static files

--- Backend Angela/todolistv1/app.js
+++ Backend Angela/todolistv1/app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine', 'ejs');  // it will looks for views folder
 app.use(bodyParser.urlencoded({extended:true}));  // it specifies that we are using body parsor soo that now we can use ha ha
+app.use(express.static("public")); // to get static files like css and all , bcz in future we are going to deal with server so need a folder publuic in which all files a server can serve ha ha
+
 var items = ["buy food","Cook food","Eat food ha ha"];
 app.get('/',(req,res)=>{
     // res.send("hello ha ha"); // response from server 
