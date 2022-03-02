d41ca45abf6a0ae9a2f27f6a9a738360a0be8db8
ketan <ketanrtd1@gmail.com>
2022-03-02 15:16:07 +0530

    embedding and relationships

--- Backend Angela/fruitsProject/app.js
+++ Backend Angela/fruitsProject/app.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { FindCursor } = require('mongoose/node_modules/mongodb');
 mongoose.connect("mongodb://localhost:27017/fruitsDB")
+mongoose.connect("mongodb://localhost:27017/peopledb")
 // until now the code is runned 
 
 //create a new schema , structure 
@@ -24,11 +25,11 @@ const Fruit = mongoose.model("Fruit",fruitsSchema); // it will make a new collec
 // what if i dont metion schema ?
 
 const fruit = new Fruit({ 
-    // name:"a",
+    name:"apple",
     rating: 4, // if rating more than allowed then error 
     review:"pretty solid as a fruit"
 });  // it will make a document out of the model of Fruit and then 4
-// fruit.save(); 
+fruit.save(); 
 // it will be saved to the database by this method
 // it will save the same thing all the time 
 // check values in terminal by mongo and going to fruits collection in fruitDB database 
@@ -67,13 +68,13 @@ const fruit = new Fruit({
 // })
 
 // Now delete that One
-Fruit.deleteOne({name :"apple"},function(err){
-    if(err) console.log(err);
-    else
-    console.log('Successfully deleted ');
+// Fruit.deleteOne({name :"apple"},function(err){
+//     if(err) console.log(err);
+//     else
+//     console.log('Successfully deleted ');
     
     
-})
+// })
 
 // to find the fruit 
 Fruit.find(function(err,fruits) {
@@ -81,7 +82,7 @@ Fruit.find(function(err,fruits) {
     console.log(err);
     else{
         // console.log(fruits); // to print all the stuff
-        mongoose.connection.close(); // to close the conection once work is over is a good practice
+        // mongoose.connection.close(); // to close the conection once work is over is a good practice
         fruits.forEach(function(element){
             console.log(element.name);
             
@@ -91,3 +92,25 @@ Fruit.find(function(err,fruits) {
     
 
 }) // from this we will get the array of objects(documents here)
+
+
+
+// Establishing relationship 
+// taken from app2 and then updated 
+
+
+
+const peopleSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    favouriteFruit:fruitsSchema
+});
+
+const PeopleModel = mongoose.model("People",peopleSchema);
+
+const people = new PeopleModel({
+    name: "Ketan",
+    age:19,
+    favouriteFruit:fruit
+});
+people.save();
