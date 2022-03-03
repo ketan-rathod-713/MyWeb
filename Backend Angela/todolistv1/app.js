34bf9f437c0324b2709a27400d0d482084564e59
ketan <ketanrtd1@gmail.com>
2022-03-03 10:55:15 +0530

    need to change custom route

--- Backend Angela/todolistv1/app.js
+++ Backend Angela/todolistv1/app.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 // const date = require(__dirname+"/date.js");  // just see it when need to export and make simple things in different file to keep it clean
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // console.log(date());
 
-mongoose.connect("mongodb://localhost:27017/todolistdb",{useNewUrlParser:true})
+mongoose.connect("mongodb://localhost:27017/todolistdb", {
+  useNewUrlParser: true,
+});
 
-// now make schema 
+// now make schema
 const itemSchema = {
-    name:String
+  name: String,
 };
-// Then make model of it 
-const Item =  mongoose.model("item",itemSchema);
+// Then make model of it
+const Item = mongoose.model("item", itemSchema);
 
 const app = express();
-app.set('view engine', 'ejs');  // it will looks for views folder
-app.use(bodyParser.urlencoded({extended:true}));  // it specifies that we are using body parsor soo that now we can use ha ha
+app.set("view engine", "ejs"); // it will looks for views folder
+app.use(bodyParser.urlencoded({ extended: true })); // it specifies that we are using body parsor soo that now we can use ha ha
 app.use(express.static("public")); // to get static files like css and all , bcz in future we are going to deal with server so need a folder publuic in which all files a server can serve ha ha
 
 // var items = ["buy food","Cook food","Eat food ha ha"];
@@ -23,82 +25,106 @@ app.use(express.static("public")); // to get static files like css and all , bcz
 
 // make a doc. out of model
 const doc1 = new Item({
-    name:"Eat the food"
-})
+  name: "Eat the food",
+});
 const doc2 = new Item({
-    name:"Eat the foods"
-})
+  name: "Eat the foods",
+});
 const doc3 = new Item({
-    name:"Eat the banana"
-})
+  name: "Eat the banana",
+});
 
+const defaultItems = [doc1, doc2, doc3];
+// New schema for other items
+const listSchema = {
+  name: String,
+  items: [itemSchema], // items with that of item schema
+};
 
-app.get('/',(req,res)=>{
-    // var day ="";
-//    let day = date.getDate();
-Item.find(function(err,items){
-    if(items.length === 0){
-        Item.insertMany([doc1,doc2,doc3]); // here callback function also 
-        res.redirect("/");  // this is what we need to do in coding :)
+const List = mongoose.model("List", listSchema);
+
+app.get("/", (req, res) => {
+  // var day ="";
+  //    let day = date.getDate();
+  Item.find(function (err, items) {
+    if (items.length === 0) {
+      Item.insertMany([doc1, doc2, doc3]); // here callback function also
+      res.redirect("/"); // this is what we need to do in coding :)
     }
-        res.render("list",{listTitle:"Today",newListItems:items});
-    })
-  
+    res.render("list", { listTitle: "Today", newListItems: items });
+  });
+});
 
-})
+app.post("/", (req, res) => {
+  var item = req.body.newItem;
 
-app.post("/",(req,res)=>{
-   var item = req.body.newItem;
+  //    if(req.body.list=="work"){
+  //        workItems.push(item);
+  //        res.redirect("/work")
+  //    } else{
+  // need to add that item in database
+  let itemdoc = new Item({
+    name: item,
+  });
+  Item.insertMany([itemdoc]);
 
-//    if(req.body.list=="work"){
-//        workItems.push(item);
-//        res.redirect("/work")
-//    } else{ 
-       // need to add that item in database 
-       let itemdoc = new Item({
-           name:item
-       })
-       Item.insertMany([itemdoc]);
+  res.redirect("/");
 
-    res.redirect("/");
+  // items.push(item); // here we are adding new item to items ha ha
+  // console.log(item);
+});
 
+app.post("/delete", (req, res) => {
+  console.log(req.body);
+  let checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, function (err) {
+    if (err) console.log(err);
+    else console.log("succes");
+  });
+  res.redirect("/");
+});
 
-    // items.push(item); // here we are adding new item to items ha ha
-    // console.log(item);
-   
-})
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "work list", newListItems: workItems });
+});
 
+app.post("/work", (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  console.log(req.body);
 
-app.post("/delete",(req,res)=>{
-    console.log(req.body);
-    let checkedItemId = req.body.checkbox;
-    Item.findByIdAndRemove(checkedItemId,function(err){
-        if(err) console.log(err);
-        else
-        console.log('succes');
-        
-    }) 
-    res.redirect("/");
-})
+  res.redirect("/work");
+});
 
-app.get("/work",(req,res)=>{
-    res.render("list",{listTitle:"work list",newListItems:workItems})
-})
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
-app.post("/work",(req,res)=>{
-    let item = req.body.newItem;
-    workItems.push(item);
-    console.log(req.body);
-    
-    res.redirect("/work");
-})
+// make dynamic route with help of express
+app.get("/:customListName", (req, res) => {
+  const customListName = req.params.customListName;
 
+  List.find({ name: customListName }, function (err, foundItems) {
+    if (!foundItems) {
+      // make a new list and then render
+      const list = new List({
+        name: customListName,
+        items: defaultItems,
+      });
+      list.save();
 
-app.get("/about",(req,res)=>{
-    res.render("about");
-})
+      res.redirect("/" + customListName);
+    } else {
+      console.log("exists");
 
-app.listen(80,()=>{
-    console.log('post started on 80 ha ha');
-    
-})
+      res.render("list", {
+        listTitle: customListName,
+        newListItems: foundItems.items,
+      });
+    }
+  });
+});
+
+app.listen(80, () => {
+  console.log("post started on 80 ha ha");
+});
